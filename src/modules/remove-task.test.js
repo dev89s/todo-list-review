bec5365113a0e237ed39f4c0359d595be26cc87a
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-04-05 21:03:22 +0300

    Add describe grouping for related tests - remove-task

--- src/modules/remove-task.test.js
+++ src/modules/remove-task.test.js
@@ -2,19 +2,59 @@ import removeTask from './remove-task.js';
 import addTask from './add-to-list.js';
 import TaskList from './tasklist.js';
 
-test('remove item', () => {
-  // Arrange
-  const taskList = new TaskList();
-  const tasks = ['homework', 'books', 'library'];
+describe('remove task function tests', () => {
+  test('remove item works', () => {
+    // Arrange
+    const taskList = new TaskList();
+    const tasks = ['homework', 'books', 'library'];
 
-  tasks.forEach((task) => {
-    addTask(taskList, task);
+    tasks.forEach((task) => {
+      addTask(taskList, task);
+    });
+
+    // Act
+    const index = 1;
+    removeTask(taskList, index);
+
+    // Asses
+    expect(taskList.list[index].index).toBe(tasks.indexOf('library') - 1);
   });
 
-  // Act
-  const index = 1;
-  removeTask(taskList, index);
+  test('remove last item works', () => {
+    // Arrange
+    const taskList = new TaskList();
+    const tasks = ['homework', 'books', 'library'];
 
-  // Asses
-  expect(taskList.list[index].description).toBe('library');
+    tasks.forEach((task) => {
+      addTask(taskList, task);
+    });
+
+    // Act
+    const index = tasks.length;
+    removeTask(taskList, index);
+    tasks.splice(tasks.length - 1, 1)
+
+    // Asses
+    tasks.forEach((task, i) => {
+      expect(taskList.list[i].description).toBe(task);
+    });
+  });
+
+  test('remove single item', () => {
+    // Arrange
+    const taskList = new TaskList();
+    const tasks = ['homework'];
+
+    tasks.forEach((task) => {
+      addTask(taskList, task);
+    });
+
+    // Act
+    const index = tasks.length;
+    removeTask(taskList, index);
+    tasks.splice(index - 1, 1);
+
+    // Asses
+    expect(taskList.list.length).toBe(tasks.length);
+  });
 });
