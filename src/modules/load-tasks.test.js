cdc659cd4fe91b5b76348221d63b42fea9efac9c
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-04-06 19:29:50 +0300

    Add more coverage for load-tasks module

--- src/modules/load-tasks.test.js
+++ src/modules/load-tasks.test.js
@@ -3,6 +3,7 @@ import TaskList from './tasklist.js';
 import addToList from './add-to-list.js';
 import loadTasksToList from './load-tasks.js';
 import { completeTask } from './status-update.js';
+import { experiments } from 'webpack';
 
 describe('test load-tasks DOM function', () => {
   test('function initiates properly and adds required elements', () => {
@@ -32,7 +33,7 @@ describe('test load-tasks DOM function', () => {
     });
   });
 
-  test('check task completion working properly', () => {
+  test('check task completion acts on tasklist', () => {
     // Arrange
     const dom = new JSDOM();
     const { document } = dom.window;
@@ -57,5 +58,130 @@ describe('test load-tasks DOM function', () => {
     const li = document.getElementById(index);
     const taskCheck = li.querySelector('.task-checkbox');
     expect(taskCheck.checked).toBe(true);
+  });
+
+  test('task completion changes page', () => {
+    // Arrange
+    const dom = new JSDOM();
+    const { document } = dom.window;
+    global.DOMParser = document.DOMParser;
+    global.document = document;
+    const todolist = document.createElement('div');
+    todolist.classList.add('todolist');
+    document.body.appendChild(todolist);
+
+    const tasklist = new TaskList();
+    const tasks = ['homework', 'shopping', 'cleaning', 'reading'];
+    tasks.forEach(((task) => {
+      addToList(tasklist, task);
+    }));
+    const index = 2;
+
+    // Act
+    loadTasksToList(tasklist);
+
+    // Asses
+    const li = document.getElementById(index);
+    const taskCheck = li.querySelector('.task-checkbox');
+    taskCheck.click();
+    expect(taskCheck.checked).toBe(true);
+    taskCheck.click();
+    expect(taskCheck.checked).toBe(false);
+  });
+
+  test('task deletion', () => {
+    // Arrange
+    const dom = new JSDOM();
+    const { document } = dom.window;
+    global.DOMParser = document.DOMParser;
+    global.document = document;
+    const todolist = document.createElement('div');
+    todolist.classList.add('todolist');
+    document.body.appendChild(todolist);
+
+    const tasklist = new TaskList();
+    const tasks = ['homework', 'shopping', 'cleaning', 'reading'];
+    tasks.forEach(((task) => {
+      addToList(tasklist, task);
+    }));
+    const index = 2;
+
+    // Act
+    loadTasksToList(tasklist);
+
+    // Asses
+    let li = document.getElementById(index);
+    const trashBtn = li.querySelector('.trash-btn');
+    trashBtn.click();
+    li = document.getElementById(index);
+    const description = li.querySelector('.task-input').value;
+    expect(description).toBe(tasks[index]);
+  });
+
+  test('textbox focusin', () => {
+    // Arrange
+    const dom = new JSDOM();
+    const { document } = dom.window;
+    global.DOMParser = document.DOMParser;
+    global.document = document;
+    const todolist = document.createElement('div');
+    todolist.classList.add('todolist');
+    document.body.appendChild(todolist);
+
+    const tasklist = new TaskList();
+    const tasks = ['homework', 'shopping', 'cleaning', 'reading'];
+    tasks.forEach(((task) => {
+      addToList(tasklist, task);
+    }));
+    const index = 2;
+
+    // Act
+    loadTasksToList(tasklist);
+
+    // Asses
+    let li = document.getElementById(index);
+    const textBox = li.querySelector('.task-input');
+    textBox.focus();
+    const threeDots = li.querySelector('.three-dots');
+    const trashBtn = li.querySelector('.trash-btn');
+    expect(threeDots.style.display).toBe('none');
+    expect(trashBtn.style.display).toBe('flex');
+  });
+
+  test('textbox focusout', () => {
+    // Arrange
+    const dom = new JSDOM();
+    const { document } = dom.window;
+    global.DOMParser = document.DOMParser;
+    global.document = document;
+    const todolist = document.createElement('div');
+    todolist.classList.add('todolist');
+    document.body.appendChild(todolist);
+
+    const tasklist = new TaskList();
+    const tasks = ['homework', 'shopping', 'cleaning', 'reading'];
+    tasks.forEach(((task) => {
+      addToList(tasklist, task);
+    }));
+    const index = 2;
+
+    // Act
+    loadTasksToList(tasklist);
+
+    // Asses
+    let li = document.getElementById(index);
+    const textBox = li.querySelector('.task-input');
+    textBox.focus();
+    textBox.value = 'something else';
+    textBox.blur();
+    const threeDots = li.querySelector('.three-dots');
+    const trashBtn = li.querySelector('.trash-btn');
+    expect(threeDots.style.display).toBe('none');
+    expect(trashBtn.style.display).toBe('flex');
+    setTimeout(() => {
+      expect(threeDots.style.display).toBe('flex');
+      expect(trashBtn.style.display).toBe('none');
+    }, 500);
+    expect(tasklist.list[index - 1].description).toBe('something else');
   });
 });
